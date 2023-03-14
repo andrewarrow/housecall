2058945b241d79a00e739f6c73a5bb402812ea58
Andrew Arrow <oneone@gmail.com>
2023-03-14 14:10:53 -0700

    more generation

--- app/appointments_controller.go
+++ app/appointments_controller.go
@@ -1,13 +1,47 @@
 package app
 
 import (
-  "github.com/andrewarrow/feedback/router"
+	"github.com/andrewarrow/feedback/router"
 )
 
 func HandleAppointments(c *router.Context, second, third string) {
-  if c.User == nil {
-    c.UserRequired = true
-    return
-  }
-  c.SendContentInLayout("appointments_index.html", nil, 200)
+	if c.User == nil {
+		c.UserRequired = true
+		return
+	}
+	if second == "" {
+		handleAppointmentsIndex(c)
+	} else if second != "" && third == "" {
+		handleAppointmentsShow(c, second)
+	} else {
+		c.NotFound = true
+	}
+}
+
+func handleAppointmentsIndex(c *router.Context) {
+	if c.Method == "GET" {
+		c.SendContentInLayout("appointments_index.html", nil, 200)
+		return
+	}
+	handleAppointmentsCreate(c)
+}
+
+func handleAppointmentsCreate(c *router.Context) {
+	c.NotFound = true
+}
+
+func handleAppointmentsShow(c *router.Context, id string) {
+	if c.Method == "GET" {
+		c.SendContentInLayout("appointments_show.html", nil, 200)
+		return
+	}
+	handleAppointmentsUpdates(c, id)
+}
+
+func handleAppointmentsUpdates(c *router.Context, id string) {
+	if c.Method == "POST" {
+		c.NotFound = true
+	} else if c.Method == "DELETE" {
+		c.NotFound = true
+	}
 }
--- app/workers_controller.go
+++ app/workers_controller.go
@@ -1,13 +1,47 @@
 package app
 
 import (
-  "github.com/andrewarrow/feedback/router"
+	"github.com/andrewarrow/feedback/router"
 )
 
 func HandleWorkers(c *router.Context, second, third string) {
-  if c.User == nil {
-    c.UserRequired = true
-    return
-  }
-  c.SendContentInLayout("workers_index.html", nil, 200)
+	if c.User == nil {
+		c.UserRequired = true
+		return
+	}
+	if second == "" {
+		handleWorkersIndex(c)
+	} else if second != "" && third == "" {
+		handleWorkersShow(c, second)
+	} else {
+		c.NotFound = true
+	}
+}
+
+func handleWorkersIndex(c *router.Context) {
+	if c.Method == "GET" {
+		c.SendContentInLayout("workers_index.html", nil, 200)
+		return
+	}
+	handleWorkersCreate(c)
+}
+
+func handleWorkersCreate(c *router.Context) {
+	c.NotFound = true
+}
+
+func handleWorkersShow(c *router.Context, id string) {
+	if c.Method == "GET" {
+		c.SendContentInLayout("workers_show.html", nil, 200)
+		return
+	}
+	handleWorkersUpdates(c, id)
+}
+
+func handleWorkersUpdates(c *router.Context, id string) {
+	if c.Method == "POST" {
+		c.NotFound = true
+	} else if c.Method == "DELETE" {
+		c.NotFound = true
+	}
 }

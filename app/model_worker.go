65182a29a2b67f622fcbdf24b9f2699b885b6bd0
Andrew Arrow <oneone@gmail.com>
2023-03-28 07:39:07 -0700

    new insert

--- app/model_worker.go
+++ app/model_worker.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/andrewarrow/feedback/router"
-	"github.com/andrewarrow/feedback/sqlgen"
 )
 
 func workersByUserIdWhere(id int64) string {
@@ -17,9 +16,7 @@ func countWorkersByUserId(c *router.Context, id int64) int64 {
 }
 
 func addWorkerWithUserId(c *router.Context, id int64, jsonParams map[string]any) {
-	jsonParams["user_id"] = id
-	model := c.FindModel("worker")
-	tableName := model.TableName()
-	sql, params := sqlgen.InsertRow(tableName, model.Fields, jsonParams)
-	c.Db.Exec(sql, params...)
+	c.Params = jsonParams
+	c.Validate("worker")
+	c.Insert("worker")
 }
